a644f9773168db56141dc05eb4bc0327adda46bc
wkia <wkia@users.noreply.github.com>
2016-10-19 00:21:57 +0300

    Implemented version working correctly with Kodi Remote.

--- plugin.video.olga/default.py
+++ plugin.video.olga/default.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/python
 
+from urlparse import parse_qsl
 import random
 import sys
-import urlparse
 import xbmcaddon
 import xbmcgui
 import xbmcplugin
@@ -24,21 +25,13 @@ __addonid__ = __addon__.getAddonInfo('id')
 ADDONVERSION = __addon__.getAddonInfo('version')
 #CWD = __addon__.getAddonInfo('path').decode("utf-8")
 
-SESSION = 'olga'
-
-log('start -----------------------------------------------------', SESSION)
-log('script version %s started' % ADDONVERSION, SESSION)
-
-#xbmc.log(str(sys.argv))
 addonUrl = sys.argv[0]
-addon_handle = int(sys.argv[1])
-args = urlparse.parse_qs(sys.argv[2][1:])
-#xbmc.log(str(args))
+addonHandle = int(sys.argv[1])
 
-xbmcplugin.setContent(addon_handle, 'video')
+#log('start -----------------------------------------------------')
+#log('script version %s started' % ADDONVERSION)
+#log(str(sys.argv))
 
-minuteCount = args.get('minutes', None)
-tvshowid = args.get('tvshowid', None)
 
 def getTvshows():
     req = {
@@ -46,16 +39,19 @@ def getTvshows():
         "method": "VideoLibrary.GetTVShows",
         "id": 1,
         "params": {
-            "properties": ["genre", "title", "originaltitle", "playcount", "file"],
+            "properties": ["genre", "title", "originaltitle", "playcount", "file", "art", "thumbnail"],
             "limits": {"start": 0, "end": 1000},
             "sort": {"order": "ascending", "method": "title"},
         }
     }
 
     rpcresp = xbmc.executeJSONRPC(json.dumps(req))
-    #xbmc.log(str(rpcresp))
+    #log(str(rpcresp))
     rpcresp = json.loads(rpcresp)
-    return rpcresp
+    if 0 < int(rpcresp['result']['limits']['end']):
+        return rpcresp['result']['tvshows']
+    else:
+        return []
 
 
 def getEpisodes(tvshowid):
@@ -75,7 +71,10 @@ def getEpisodes(tvshowid):
     rpcresp = xbmc.executeJSONRPC(json.dumps(req))
     #xbmc.log(str(rpcresp))
     rpcresp = json.loads(rpcresp)
-    return rpcresp
+    if 0 < int(rpcresp['result']['limits']['end']):
+        return rpcresp['result']['episodes']
+    else:
+        return []
 
 
 def getDuration(item):
@@ -89,51 +88,66 @@ def getDuration(item):
     return duration
 
 
-if tvshowid is None:
-
+def listTvshows():
     tvshows = getTvshows()
-    found = 0 < int(tvshows['result']['limits']['end'])
-    if found :
-        for e in tvshows['result']['tvshows']:
-            url = build_url(addonUrl, {'tvshowid': e['tvshowid']})
-            li = xbmcgui.ListItem(e['title'], iconImage='DefaultFolder.png')
-            xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+    listing = []
+    for e in tvshows:
+        #log(str(e))
+        li = xbmcgui.ListItem(e['title'], iconImage='DefaultFolder.png')
+        url = build_url(addonUrl, {'action': 'choosetime', 'tvshowid': e['tvshowid']})
+        li.setInfo('video', {'title': e['title']})
+        art = {}
+        if 'thumbnail' in e:
+            art['thumb'] = e['thumbnail']
+        if 'art' in e:
+            if 'banner' in e['art']:
+                art['banner'] = e['art']['banner']
+            if 'fanart' in e['art']:
+                art['fanart'] = e['art']['fanart']
+        li.setArt(art)
+        isFolder = True
+        listing.append((url, li, isFolder))
 
-elif minuteCount is None:
+    xbmcplugin.addDirectoryItems(addonHandle, listing, len(listing))
+    xbmcplugin.addSortMethod(addonHandle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    xbmcplugin.endOfDirectory(addonHandle)
 
-    tvshowid = int(tvshowid[0])
 
-    url = build_url(addonUrl, {'tvshowid': tvshowid, 'minutes': 30})
+def listItems(tvshowid):
+    url = build_url(addonUrl, {'action': 'play', 'tvshowid': tvshowid, 'minutes': 30})
     li = xbmcgui.ListItem('30 minutes', iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
-    url = build_url(addonUrl, {'tvshowid': tvshowid, 'minutes': 60})
+    xbmcplugin.addDirectoryItem(handle=addonHandle, url=url, listitem=li, isFolder=False)
+    url = build_url(addonUrl, {'action': 'play', 'tvshowid': tvshowid, 'minutes': 60})
     li = xbmcgui.ListItem('60 minutes', iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
-    url = build_url(addonUrl, {'tvshowid': tvshowid, 'minutes': 90})
+    xbmcplugin.addDirectoryItem(handle=addonHandle, url=url, listitem=li, isFolder=False)
+    url = build_url(addonUrl, {'action': 'play', 'tvshowid': tvshowid, 'minutes': 90})
     li = xbmcgui.ListItem('90 minutes', iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
-    url = build_url(addonUrl, {'tvshowid': tvshowid, 'minutes': 0})
+    xbmcplugin.addDirectoryItem(handle=addonHandle, url=url, listitem=li, isFolder=False)
+    url = build_url(addonUrl, {'action': 'play', 'tvshowid': tvshowid, 'minutes': 0})
     li = xbmcgui.ListItem('Custom length...', iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
+    xbmcplugin.addDirectoryItem(handle=addonHandle, url=url, listitem=li, isFolder=False)
 
-else:
+    xbmcplugin.addSortMethod(addonHandle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    xbmcplugin.endOfDirectory(addonHandle)
 
+
+def playVideo(tvshowid, minuteCount):
     filename = xbmc.translatePath("special://temp/" + __addonid__)
-    #log('acquiring lock for %s...' % filename, SESSION)
+    #log('acquiring lock for %s...' % filename)
     lock = FileLock(filename)
     if not lock.acquire():
-        log('lock for %s is busy' % filename, SESSION)
+        log('lock for %s is busy' % filename)
         sys.exit(0)
-    #log('acquired lock for %s' % filename, SESSION)
+    #log('acquired lock for %s' % filename)
 
-    tvshowid = int(tvshowid[0])
-    minuteCount = int(minuteCount[0])
+    tvshowid = int(tvshowid)
+    minuteCount = int(minuteCount)
 
     if 0 == minuteCount:
         user_keyboard = xbmc.Keyboard()
         user_keyboard.setHeading('Set custom playlist length in minutes')  # __language__(30001))
         user_keyboard.setHiddenInput(False)
-        user_keyboard.setDefault("60")
+        user_keyboard.setDefault("120")
         user_keyboard.doModal()
         if user_keyboard.isConfirmed():
             # int() throws an exception if falied
@@ -143,52 +157,66 @@ else:
         else:
             sys.exit(0)
 
-    # Generate random playlist
+    secondCount = minuteCount * 60
+    random.seed()
+
+    #log('--- stopping player')
     xbmc.Player().stop()
     xbmc.sleep(500)
-    random.seed()
+
+    #log('--- creating playlist')
+    episodes = getEpisodes(tvshowid)
+
     playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
     playlist.clear()
-    secondCount = minuteCount * 60
 
-    episodes = getEpisodes(tvshowid)
-    found = 0 < int(episodes['result']['limits']['end'])
-    if found :
-        episodes = episodes['result']['episodes']
-        num = int(len(episodes) / 2)
-        episodesRecent = episodes[num:]
-        episodes = episodes[:num]
+    xlistitem = xbmcgui.ListItem('video title')
+    xlistitem.setInfo("video", infoLabels={"Title": 'video title'})
+    #xlistitem.setProperty('IsPlayable', 'true')
 
-        while 0 < len(episodes):
-            n = random.randint(0, len(episodes) - 1)
-            item = episodes.pop(n)
-            if 0 == len(episodes) and 0 < len(episodesRecent):
-                num = int(len(episodesRecent)/2 + 1)
-                episodes = episodesRecent[:num]
-                episodesRecent = episodesRecent[num:]
+    num = int(len(episodes) / 2)
+    episodesRecent = episodes[num:]
+    episodes = episodes[:num]
 
-            #xbmc.log(str(item))
-            duration = getDuration(item)
-            if 0 == duration:
-                continue
-            secondCount = secondCount - duration
-            #log("duration %d, seconds remain %d, %s" % (int(duration), int(secondCount), item['title']), SESSION)
-            if secondCount <= 0 or secondCount < duration / 2:
-                break
+    while 0 < len(episodes):
+        n = random.randint(0, len(episodes) - 1)
+        item = episodes.pop(n)
+        if 0 == len(episodes) and 0 < len(episodesRecent):
+            num = int(len(episodesRecent)/2 + 1)
+            episodes = episodesRecent[:num]
+            episodesRecent = episodesRecent[num:]
 
-            xlistitem = xbmcgui.ListItem(item['title'])
-            xlistitem.setInfo("video", infoLabels={"Title": item['title']})
-            xlistitem.setArt({'thumb': item['thumbnail']})  # , 'fanart': thumb})
-            playlist.add(item['file'], xlistitem)
+        #xbmc.log(str(item))
+        duration = getDuration(item)
+        if 0 == duration:
+            continue
+        secondCount = secondCount - duration
+        #log("duration %d, seconds remain %d, %s" % (int(duration), int(secondCount), item['title']))
+        if secondCount <= 0 or secondCount < duration / 2:
+            break
 
-    else:
-        # No series found
-        raise Exception("No series found")
+        xlistitem = xbmcgui.ListItem(item['title'])
+        xlistitem.setInfo("video", infoLabels={"Title": item['title']})
+        xlistitem.setArt({'thumb': item['thumbnail']})  # , 'fanart': thumb})
+        playlist.add(item['file'], xlistitem)
 
-    #xbmc.executebuiltin("ActivateWindow(%d)" % (10000,))
+    #log('--- starting player')
     playlist.shuffle()
     xbmc.Player().play(playlist)
 
 
-xbmcplugin.endOfDirectory(addon_handle)
-log('end -----------------------------------------------------', SESSION)
+def dispatch(param):
+    params = dict(parse_qsl(param[1:]))
+    if params:
+        if params['action'] == 'choosetime':
+            listItems(params['tvshowid'])
+        elif params['action'] == 'play':
+            playVideo(params['tvshowid'], params['minutes'])
+    else:
+        listTvshows()
+
+
+if __name__ == '__main__':
+    dispatch(sys.argv[2])
+
+#log('end -----------------------------------------------------')
--- plugin.video.olga/logging.py
+++ plugin.video.olga/logging.py
@@ -6,9 +6,9 @@ import xbmcaddon
 __addon__ = xbmcaddon.Addon()
 __addonid__ = __addon__.getAddonInfo('id')
 
-def log(txt, session):
+def log(txt):
     if isinstance(txt, str):
         txt = txt.decode("utf-8")
-    message = u'%s - %s: %s' % (__addonid__, session, txt)
+    message = u'%s: %s' % (__addonid__, txt)
     xbmc.log(msg=message.encode("utf-8"))
 
